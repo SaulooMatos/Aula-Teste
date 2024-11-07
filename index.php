d742683728918f0d117f55268ca78bfa1e8600f9
Saulo Matos <matossaulo.h@gmail.com>
2024-11-06 22:15:35 -0300

    index.php

---
+++ index.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu site da Apple</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #a3c1c9;
+            text-align: center;
+            color: #333;
+        }
+
+        header {
+            background-color: #333;
+            color: #fff;
+            padding: 20px 0;
+        }
+
+        header h1 {
+            margin: 0;
+            font-size: 1.5em;
+            padding: 0 20px;
+            line-height: 1.5;
+        }
+
+        nav {
+            margin: 30px 0;
+        }
+
+        nav a {
+            background-color: #fff;
+            color: #030303;
+            padding: 15px 30px;
+            margin: 0 10px;
+            text-decoration: none;
+            border-radius: 5px;
+            transition: background-color 0.3s, color 0.3s;
+        }
+
+        nav a:hover {
+            background-color: #333;
+            color: #fff;
+        }
+
+        section {
+            padding: 20px;
+        }
+
+        h2 {
+            font-size: 1.8em;
+            margin-bottom: 20px;
+        }
+
+        .Produtos {
+            font-size: 36px;
+            padding: 15px 30px;
+            font-family: Arial, sans-serif;
+
+        }
+
+        img {
+            max-width: 100%;
+            height: auto;
+            margin: 10px 0;
+            border: 2px solid #333;
+            transition: transform 0.3s, box-shadow 0.3s;
+        }
+
+        img:hover {
+            transform: scale(1.05);
+            box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3);
+        }
+
+        footer {
+            background-color: #000000;
+            color: #fff;
+            padding: 10px 0;
+            bottom: 0;
+            width: 100%;
+        }
+    </style>
+</head>
+
+<body>
+
+    <header>
+        <h1>SITE INFORMATIVO SOBRE A APPLE</h1>
+        <h1>Apple Inc. é uma das empresas de tecnologia mais influentes do mundo, conhecida por seus produtos inovadores
+            como iPhone, Mac e muito mais</h1>
+
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="processar.php">Banco</a>
+            <a href="formulario.php">Formulário</a>
+            <a href="produtos.php">Produtos</a>
+        </nav>
+
+    </header>
+
+
+
+    <section>
+        <h2>A Apple Inc. é uma das empresas de tecnologia mais influentes e reconhecidas globalmente. Fundada em 1976
+            por Steve Jobs,
+            Steve Wozniak e Ronald Wayne, a Apple começou como uma fabricante de computadores pessoais, mas rapidamente
+            se expandiu
+            para uma ampla gama de produtos inovadores, incluindo o iPhone, iPad, Mac, Apple Watch e Apple TV. A empresa
+            é conhecida
+            por seu design elegante, inovação tecnológica e um ecossistema integrado de hardware, software e serviços.
+            Com uma base
+            de clientes leais e uma presença global, a Apple continua a liderar o mercado com produtos que combinam alta
+            funcionalidade
+            com uma experiência de usuário intuitiva e refinada</h2>
+
+        <a href="https://www.apple.com" target="_blank">
+            <img src="Imagem/foto1.jpg" alt="Foto 1">
+        </a>
+        <a href="https://www.apple.com" target="_blank">
+            <img src="Imagem/foto2.jpeg" alt="Foto 2">
+        </a>
+
+        <h2 class="Produtos">PRODUTOS APPLE</h2>
+        <a href="https://www.apple.com" target="_blank">
+            <img src="Imagem/foto3.jfif" alt="Foto 3">
+        </a>
+        <a href="https://www.apple.com" target="_blank">
+            <img src="Imagem/foto4.jfif" alt="Foto 4">
+        </a>
+        <a href="https://www.apple.com" target="_blank">
+            <img src="Imagem/foto5.jpg" alt="Foto 5">
+        </a>
+
+        <h2 class="Produtos">APPLE INTELLIGENCE </h2>
+        <a href="https://www.apple.com" target="_blank">
+            <img src="Imagem/foto6.avif" alt="Foto 6">
+        </a>
+        <a href="https://www.apple.com" target="_blank">
+            <img src="Imagem/foto7.jfif" alt="Foto 7">
+        </a>
+        <a href="https://www.apple.com" target="_blank">
+            <img src="Imagem/foto8.jpg" alt="Foto 8">
+        </a>
+
+        <h2 class="Produtos">APPLE LOGO</h2>
+        <a href="https://www.apple.com" target="_blank">
+            <img src="Logo/logo1.jpg" alt="logo1">
+        <a href="https://www.apple.com" target="_blank">
+            <img src="Logo/logo2.jpg" alt="logo2">
+        <a href="https://www.apple.com" target="_blank">
+            <img src="Logo/logo3.jpg" alt="logo3">
+
+    </section>
+
+    <footer>
+        <p>&copy; 2024 Saulo - Site Apple</p>
+    </footer>
+
+</body>
+
+</html>
